2d2c97b128245cbb726bbecf718be4e18d2649c9
yangxu <yangxu_qzhit@outlook.com>
2019-12-17 17:51:56 +0800

    添加组合实体模式。

---
+++ src/J2EE/compositeentity/DependentObject2.java
@@ -0,0 +1,19 @@
+package J2EE.compositeentity;
+
+/**
+ * @ClassName DependentObject2
+ * @Description
+ * @Author yangxu
+ * @Date 2019-12-17 17:39
+ **/
+public class DependentObject2 {
+    private String data;
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
